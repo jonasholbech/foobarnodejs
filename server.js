54daabfcf1445f1abe764103ffd92d65829bf366
Jonas Holbech <jh@jonasholbech.dk>
2020-05-03 17:40:00 +0200

    CORS enabled

--- server.js
+++ server.js
@@ -5,6 +5,16 @@ const bodyParser = require("body-parser");
 var app = express();
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE"); //OPTIONS
+  next();
+});
+
 const { FooBar } = require("./src/foobar");
 //console.log(FooBar);
 var todos = [
